3f4ca511ef75812fd91d3b9553ff94b22d0fbf90
Nattanicha <nattanicha@bakertilly.co.th>
2025-03-24 13:23:44 +0700

    init project

--- src/main/java/com/auth_service/auth_service/AuthServiceApplication.java
+++ src/main/java/com/auth_service/auth_service/AuthServiceApplication.java
@@ -2,8 +2,12 @@ package com.auth_service.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaRepositories
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
